a6b9b624094fe8aab97287183f1ca8511ee484d9
Sobiech <mikolajmikolaj090302@gmail.com>
2025-09-22 19:26:50 +0200

    init

---
+++ src/main/java/ovh/sobiech/EstrogenApplication.java
@@ -0,0 +1,13 @@
+package ovh.sobiech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstrogenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstrogenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ovh/sobiech/EstrogenApplicationTests.java
@@ -0,0 +1,13 @@
+package ovh.sobiech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstrogenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
